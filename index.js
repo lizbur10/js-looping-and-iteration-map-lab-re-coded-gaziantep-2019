d61d188614072a31c006061891606fc3561d61a6
Liz Burton <liz.burton147@gmail.com>
2019-09-07 15:37:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file.
-function lowerCaseDrivers() {
-  
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(driver) {
+    driver.toLowerCase();
+  })
 }
